760a60185bded81947c193b2ec1e97503e778d30
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-07-12 00:28:43 +0200

    commit inicial

---
+++ main.js
@@ -0,0 +1,6 @@
+//(document.body) conecta el Js con HTML
+console.log (document.body);
+let cards = document.getElementsByClassName("card")
+console.log(cards);
+alert("Igresa el nombre correcto de la marca de los autos en pantalla");
+
