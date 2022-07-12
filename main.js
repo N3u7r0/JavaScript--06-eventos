28cdbb5964332e8b948c3254d8938aa80c7dc203
N3u7r0 <brian_sabatini1993@hotmail.com>
2022-07-12 01:24:52 -0300

    eventos con botones 2.0

--- main.js
+++ main.js
@@ -27,19 +27,38 @@ function respuestaToyotaTres() {
 let btnNissanUno = document.getElementById("btn__Nissan__Uno");
 btnNissanUno.addEventListener("click", respuestaNissanUno);
 function respuestaNissanUno() {
-    console.log("Respuesta correcta");
-    alert("!!!Correcto!!!");
+    console.log("Respuesta incorrecta");
+    alert("!!!Incorrecto!!!, es un NISSAN");
 }
 let btnNissanDos = document.getElementById("btn__Nissan__Dos");
 btnNissanDos.addEventListener("click", respuestaNissanDos);
 function respuestaNissanDos() {
-    console.log("Respuesta incorrecta");
-    alert("!!!incorrecto!!!, es marca TOYOTA");
+    console.log("Respuesta correcta");
+    alert("!!!Correcto!!!");
 }
-let btnNisanTres = document.getElementById("btn__Nissan__Tres");
+let btnNissanTres = document.getElementById("btn__Nissan__Tres");
 btnNissanTres.addEventListener("click", respuestaNissanTres);
 function respuestaNissanTres() {
     console.log("Respuesta incorrecta");
-    alert("!!!incorrecto!!!, es marca TOYOTA")
+    alert("!!!incorrecto!!!, es un NISSAN");
 }
 
+/*Subaru*/
+let btnSubaruUno = document.getElementById("btn__Subaru__Uno");
+btnSubaruUno.addEventListener("click", respuestaSubaruUno);
+function respuestaSubaruUno() {
+    console.log("Respuesta incorrecta");
+    alert("!!!Incorrecto!!!, es un SUBARU!");
+}
+let btnSubaruDos = document.getElementById("btn__Subaru__Dos");
+btnSubaruDos.addEventListener("click", respuestaSubaruDos);
+function respuestaSubaruDos() {
+    console.log("Respuesta incorrecta");
+    alert("!!!Incorrecto!!!, es un SUBARU...");
+}
+let btnSubaruTres = document.getElementById("btn__Subaru__Tres");
+btnSubaruTres.addEventListener("click", respuestaSubaruTres);
+function respuestaSubaruTres() {
+    console.log("Respuesta correcta");
+    alert("!!!correcto!!");
+}
